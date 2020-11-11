cf4fc9381ffdfa0dd579c7b16445ca19cf5ed3af
Dilmer Valecillos <dilmervst@gmail.com>
2020-11-10 23:49:53 -0700

    Initial Project

---
+++ Assets/Scripts/PlaceOnMesh.cs
@@ -0,0 +1,106 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+ 
+public class PlaceOnMesh : MonoBehaviour 
+{
+    [SerializeField] 
+    private ARMeshManager meshManager = null;
+
+    [SerializeField] 
+    private ARSessionOrigin origin = null;
+
+    [SerializeField] 
+    private Material invisibleMaterial = null;
+
+    [SerializeField] 
+    private Material visibleMaterial = null;
+
+    [SerializeField] 
+    private GameObject[] prefabs;
+
+    [SerializeField] 
+    private Slider prefabSize;
+    
+    [SerializeField] 
+    private TextMeshProUGUI prefabSizeLabel = null;
+
+    [SerializeField] 
+    private TextMeshProUGUI visibilityStatus = null;
+
+    [SerializeField] 
+    private LayerMask layersToInclude;
+
+    private bool IsVisible { get; set; } = true;
+    
+    private void Awake() 
+    {
+        prefabSize.onValueChanged.AddListener(PrefabSizeChanged);
+    }
+
+    public void PrefabSizeChanged(float newSize)
+    {
+        prefabSizeLabel.text = $"SIZE {newSize}";
+    }
+
+    public void ToggleMesh()
+    {
+        var meshes = meshManager.meshes;
+        var meshPrefabRenderer = meshManager.meshPrefab.GetComponent<MeshRenderer>();
+
+        if(IsVisible)
+        {
+            meshPrefabRenderer.material = invisibleMaterial;
+
+            foreach(var mesh in meshes)
+            {
+                mesh.GetComponent<MeshRenderer>().material = invisibleMaterial;
+            }
+
+            IsVisible = false;
+            visibilityStatus.text = "Mesh Visibility (ON)";
+        }
+        else
+        {
+            meshPrefabRenderer.material = visibleMaterial;
+
+            foreach(var mesh in meshes)
+            {
+                mesh.GetComponent<MeshRenderer>().material = visibleMaterial;
+            }
+
+            IsVisible = true;
+            visibilityStatus.text = "Mesh Visibility (OFF)";
+        }
+    }
+
+    void Update() 
+    {
+        for (int i = 0; i < Input.touchCount; i++) 
+        {
+            var touch = Input.GetTouch(i);
+            var touchPhase = touch.phase;
+
+            if (touchPhase == TouchPhase.Began || touchPhase == TouchPhase.Moved) 
+            {
+                var ray = origin.camera.ScreenPointToRay(touch.position);
+                var hasHit = Physics.Raycast(ray, out var hit, float.PositiveInfinity, layersToInclude);
+
+                if (hasHit) 
+                {
+                    GameObject newObject = null;
+                    Vector3 scale = Vector3.one * prefabSize.value;
+                    Quaternion newObjectRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                    
+                    int selector = Random.Range(0, prefabs.Length);
+                    newObject = GameObject.Instantiate(prefabs[selector], hit.point, newObjectRotation);
+                   
+
+                    newObject.transform.localScale = scale;
+                    newObject.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/ThirdParty/AlignedGames/Polygonal Foliage Asset Package/Shaders/Cloud.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/Cloud" {
+    Properties {
+        _Color ("Main Color", Color) = (1,1,1,1)
+        _MainAlpha ("Main Alpha", Range(0.0,1.0)) = 1.0
+        _RimColor ("Rim Color", Color) = (0.26,0.19,0.16,0.0)
+        _RimPower ("Rim Power", Range(0.5,8.0)) = 3.0
+    }
+   
+    SubShader {
+       
+            Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+           
+            CGPROGRAM
+           
+            #pragma surface surf Lambert alpha
+           
+            struct Input {
+                float3 _Color;
+                float3 viewDir;
+            };
+           
+            float4 _Color;
+            float4 _RimColor;
+            float _RimPower;
+            float _MainAlpha;
+           
+            void surf (Input IN, inout SurfaceOutput o) {
+               
+                o.Albedo = _Color;
+               
+                half rim = 1.0 - saturate(dot (normalize(IN.viewDir), o.Normal));
+                o.Emission = _RimColor.rgb * pow (rim, _RimPower);
+               
+                o.Alpha = _MainAlpha;
+            }
+           
+            ENDCG
+           
+    }
+   
+    FallBack "Diffuse"
+}
+ 
